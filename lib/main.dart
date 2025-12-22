c27e3825995b9f2ecb863a9bbf2ff035e4e3238f
David NTB <davidntb7000@gmail.com>
2025-12-23 02:58:58 +0700

    First Commit + Skeleton App

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'screens/login_screen.dart';
+
+void main() {
+  runApp(TaskMapApp());
+}
+
+class TaskMapApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'TaskMap',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: LoginScreen(),
+    );
+  }
+}
---
+++ lib/screens/task_list_screen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import '/widgets/bottom_nav.dart';
+import 'add_task_screen.dart';
+
+class TaskListScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Task List')),
+      body: Center(
+        child: ElevatedButton(
+          child: Text('Daftar Task'),
+          onPressed: () {
+            Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(builder: (context) => AddTaskScreen()),
+            );
+          },
+        ),
+      ),
+      bottomNavigationBar: BottomNav(currentIndex: 0),
+    );
+  }
+}
